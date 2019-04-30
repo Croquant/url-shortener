fa35f40e8769f362d31a5532b1d11b3468e2bc54
Croquant <maximedesmet@yahoo.com>
2019-04-29 22:13:46 -0700

    added index page with pug

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const express	= require("express");
 const app		= express();
 const port 		= 8080;
 
+// set pug as view engine
+app.set("view engine", "pug");
+
+// render index page at "/"
 app.get("/", (req, res) => {
-	res.send("Hellow World");
+	res.render("index", { title: "index" } );
 });
 
+// start server
 app.listen(port, () => {
 	console.log("listening on port " + port);
 });
