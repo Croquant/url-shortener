c72d55a50de416c4ba09b9c893c6b734f6e11f01
Croquant <maximedesmet@yahoo.com>
2019-04-29 22:21:32 -0700

    added post request retriever

--- server.js
+++ server.js
@@ -3,12 +3,23 @@ const express	= require("express");
 const app		= express();
 const port 		= 8080;
 
+const bodyParser = require("body-parser");
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+	extended: false
+})); 
+
 // set pug as view engine
 app.set("view engine", "pug");
 
 // render index page at "/"
 app.get("/", (req, res) => {
 	res.render("index", { title: "index" } );
+});
+
+// get post requests at /newlink
+app.post("/newlink", (req, res) => {
+	res.send(req.body);
 });
 
 // start server
